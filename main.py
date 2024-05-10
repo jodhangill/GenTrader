5960f5c12062756126336eea26b8f83ccf84527e
Jodhan Gill <jga145@sfu.ca>
2024-05-09 18:47:43 -0700

    Added functionality for saving optimized parameters

--- main.py
+++ main.py
@@ -1,43 +1,56 @@
 from ParameterOptimizer import *
 import yfinance as yf
+from strategies import NeuralNetwork
+import json
 
 if __name__ == '__main__':
 
     # Choose strategy
-    strategy = bt.strategies.MA_CrossOver
+    strategy = NeuralNetwork.NeuralNetwork
 
-    # Get parameters to optimize
+    # Load last best parameters to optimize
+    file_path = "params.json"
     params = {}
-    for key, value in strategy.params.__dict__.items():
-        if isinstance(value, (int, float)):  # Filter out non-optimizable parameters
-            params[key] = value
+    with open(file_path, 'r') as file:
+        params = json.load(file)
+    if not params:
+        for key, value in strategy.params.__dict__.items():
+            if isinstance(value, (int, float)):  # Filter out non-optimizable parameters
+                params[key] = value
 
     # Create constraints on parameters (Optional)
     constraints = {
-        'fast': [lambda x: x > 0],
-        'slow': [lambda x: x > 0],
+        # No constraints for neural net
     }
 
     # Choose one or more datasets
     print("Downloading data...")
-    spy1 = yf.download('SPY', '2002-01-01', '2006-4-30')
-    spy2 = yf.download('SPY', '2005-01-01', '2007-12-31')
-    datas = [spy1, spy2]
+    data1 = yf.download('SPY', '2007-12-01', '2024-01-30')
+    #data2 = yf.download('BTC-USD', '2020-12-01', '2022-8-30')
+    datas = [data1]
 
     # Set weights of each dataset
     weights = [
-        0.6, # 60% of evaluation is based on spy1
-        0.4  # 40% of evaluation is based on spy2
+        0.6, # 60% of evaluation is based on data1
+        #0.4  # 40% of evaluation is based on data2
     ]
 
     opt = ParameterOptimizer(
         strategy=strategy, 
         weights=weights,
         datas=datas, 
-        cash=5000, # Starting cash for backtesting strategies
+        cash=1000000, # Starting cash for backtesting strategies
         generation_count=5, # Number on generations before stopping optimization
         population=10, # Number of parameter sets in each generation
-        top_n=4 # Number of parameter sets to select from each generation
+        top_n=1, # Number of parameter sets to select from each generation
+        base_mutation_std=0.1
     )
+
     best_params = opt.optimize_parameters(params)
-    opt.plot(best_params, spy1)
+
+    # Save best parameter set (disabled for testing)
+    #file_path = "params.json"
+    #with open(file_path, "w") as file:
+    #    json.dump(best_params, file)
+    
+    opt.plot(best_params, data1)
